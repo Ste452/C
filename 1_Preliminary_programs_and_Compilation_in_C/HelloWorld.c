4597cec2728c4da58af9fe5fc294bd854de7e309
Steve <falecomstevevaz@gmail.com>
2024-11-22 22:25:53 -0300

    Refactoring the package and name of the main.c for HelloWorld and adding a comments in the first class and create the new practical class

---
+++ 1_Preliminary_programs_and_Compilation_in_C/HelloWorld.c
@@ -0,0 +1,13 @@
+
+// Directive on C preprocessor before the program has compiled
+#include <stdio.h>
+
+// Starting a main function for returning that is inside in this function
+int main() {
+
+    // "printf" is a command for output a variable, string, or even a function in the same main function
+    printf("Hello world!\n");
+
+
+    return 0; // Indicates that the program completed successfully
+}
